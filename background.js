6c0dab87f212f65ff600dbfb6ca5bad97aeb3c82
Istallia <55042065+istallia@users.noreply.github.com>
2021-01-19 10:38:50 +0900

    [Fix] 音量の同期が初回のみ行われる問題を修正

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 /* --- 各種パラメータの読み込み --- */
 if (typeof browser === 'undefined') browser = chrome;
-let ista_volume_bgm = Number(localStorage.getItem('ista_volume_bgm') || '1');
-let ista_volume_se  = Number(localStorage.getItem('ista_volume_se') || '1');
+let ista_volume_bgm = Number(localStorage.getItem('ista_volume_bgm') || '0.5');
+let ista_volume_se  = Number(localStorage.getItem('ista_volume_se') || '0.5');
 
 
 /* --- 音量の取得 --- */
--- script-search.js
+++ script-search.js
@@ -1,13 +1,11 @@
 /* --- 各種パラメータの読み込み＆初期設定 --- */
 if (typeof browser === 'undefined') browser = chrome;
-if (sessionStorage.getItem('ista_volume_bgm') === null) {
-	browser.runtime.sendMessage({ctrl : 'get-volume'}, params => {
-		sessionStorage.setItem('ista_volume_bgm', params['volume_bgm']);
-		sessionStorage.setItem('ista_volume_se' , params['volume_se']);
-		ista_volume_bgm = Number(sessionStorage.getItem('ista_volume_bgm') || '1');
-		ista_volume_se  = Number(sessionStorage.getItem('ista_volume_se') || '1');
-	});
-}
+browser.runtime.sendMessage({ctrl : 'get-volume'}, params => {
+	sessionStorage.setItem('ista_volume_bgm', params['volume_bgm']);
+	sessionStorage.setItem('ista_volume_se' , params['volume_se']);
+	ista_volume_bgm = Number(sessionStorage.getItem('ista_volume_bgm') || '1');
+	ista_volume_se  = Number(sessionStorage.getItem('ista_volume_se') || '1');
+});
 let ista_volume_bgm      = Number(sessionStorage.getItem('ista_volume_bgm') || '1');
 let ista_volume_se       = Number(sessionStorage.getItem('ista_volume_se') || '1');
 let ista_audio_obj       = [];

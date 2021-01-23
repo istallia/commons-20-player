513903342ac8e647f4543c84cd7b771c884c8ac9
Istallia <55042065+istallia@users.noreply.github.com>
2021-01-23 20:57:04 +0900

    [Modify] persistent = false

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
-/* --- 各種パラメータの読み込み --- */
+/* --- browserの読み込み --- */
 if (typeof browser === 'undefined') browser = chrome;
-let ista_volume_bgm = Number(localStorage.getItem('ista_volume_bgm') || '100');
-let ista_volume_se  = Number(localStorage.getItem('ista_volume_se') || '100');
 
 
 /* --- 音量の取得 --- */
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	ista_volume_bgm = Number(localStorage.getItem('ista_volume_bgm') || '100');
+	ista_volume_se  = Number(localStorage.getItem('ista_volume_se') || '100');
 	if (request.ctrl === 'get-volume') {
 		sendResponse({
 			volume_bgm : ista_volume_bgm,

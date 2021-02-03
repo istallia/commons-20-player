039627bb4a0ab8a1a9c6b305a02c0eb42ee76005
Istallia <55042065+istallia@users.noreply.github.com>
2021-02-03 17:44:01 +0900

    [Fix] ユーザ投稿作品ページでAuto Pager系拡張機能に追従できてない問題を修正

--- script-player.js
+++ script-player.js
@@ -112,7 +112,7 @@ let ista_put_func = () => {
 setTimeout(ista_put_func, 0);
 setTimeout(ista_put_func, 1000);
 window.addEventListener('load', ista_put_func);
-const target = document.querySelector("#index_box");
+const target = document.querySelector("#index_box, #index_content");
 if (target !== null) {
 	const observer = new MutationObserver(records => {
 		if (records[0].addedNodes.length > 0) setTimeout(ista_put_func, 0);

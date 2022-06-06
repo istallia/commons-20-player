6b99447ce36b9895599d59480ff20ef82406e293
Istallia <55042065+istallia@users.noreply.github.com>
2022-06-06 20:43:59 +0900

    [Replace] localStorage → Storage API 移行 (引っ越し処理付き)

--- background.js
+++ background.js
@@ -21,23 +21,28 @@ if (typeof browser === 'undefined') browser = chrome;
 
 /* --- 音量の取得など --- */
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	ista_volume_master = Number(localStorage.getItem('ista_volume_master') || '100');
-	ista_volume_bgm    = Number(localStorage.getItem('ista_volume_bgm')    || '100');
-	ista_volume_se     = Number(localStorage.getItem('ista_volume_se')     || '100');
 	if (request.ctrl === 'get-volume') {
-		sendResponse({
-			volume_master : ista_volume_master,
-			volume_bgm    : ista_volume_bgm,
-			volume_se     : ista_volume_se
+		browser.storage.local.get(['ista_volume_master', 'ista_volume_bgm', 'ista_volume_se'], data => {
+			if (!data['ista_volume_bgm'] && localStorage.getItem('ista_volume_bgm')) {
+				browser.storage.local.set({
+					volume_master : Number(localStorage.getItem('ista_volume_master')),
+					volume_bgm    : Number(localStorage.getItem('ista_volume_bgm')),
+					volume_se     : Number(localStorage.getItem('ista_volume_se'))
+				});
+			}
+			sendResponse({
+				volume_master : Number(data['ista_volume_master'] || localStorage.getItem('ista_volume_master') || '100'),
+				volume_bgm    : Number(data['ista_volume_bgm']    || localStorage.getItem('ista_volume_bgm')    || '100'),
+				volume_se     : Number(data['ista_volume_se']     || localStorage.getItem('ista_volume_se')     || '100')
+			});
 		});
 		return true;
 	} else if (request.ctrl === 'set-volume') {
-		ista_volume_master = Number(request.master);
-		ista_volume_bgm    = Number(request.bgm);
-		ista_volume_se     = Number(request.se);
-		localStorage.setItem('ista_volume_master', ista_volume_master);
-		localStorage.setItem('ista_volume_bgm'   , ista_volume_bgm);
-		localStorage.setItem('ista_volume_se'    , ista_volume_se);
+		browser.storage.local.set({
+			volume_master : Number(request.master),
+			volume_bgm    : Number(request.bgm),
+			volume_se     : Number(request.se)
+		});
 		return;
 	} else if (request.ctrl === 'update-title') {
 		browser.runtime.sendMessage(request);
